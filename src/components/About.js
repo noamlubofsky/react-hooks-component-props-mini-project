794728a7cee548c1ff7644d269d5c8980ed23e7e
github username <github email address>
2021-06-29 19:34:04 -0400

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About({ image = "https://via.placeholder.com/215", about}) {
+    return(
+    <aside>
+        <img src={image} alt="blog logo"/>
+        <p>{about}</p>
+    </aside>
+    )}
+
+export default About
+
+
+
+
+// a <aside> element with the following elements inside:
+// a <img> element, with the src set to an image passed as a prop
+// the <img> element should use this placeholder image as a default value for the prop if no prop is passed in: "https://via.placeholder.com/215"
+// the image should also be accessible! Give it an alt attribute of "blog logo"
+// a <p> element, with the about text for the blog passed in as a prop
